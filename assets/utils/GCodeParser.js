0bea7b51887637f25345e69ecd2f95e9f5b0d114
Matti Eiden <snaipperi@gmail.com>
2017-08-01 01:14:52 +0300

    WIP on gcode parser

--- assets/utils/GCodeParser.js
+++ assets/utils/GCodeParser.js
@@ -16,6 +16,96 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * GParser takes line(s) of GCode
+ */
+import Enum from 'es6-enum';
+
+const regexClean = /[^\dGXYZIJKF.+-]/gi;
+
+/**
+ * @property {Symbol} DWELL
+ * @property {Symbol} RETURN_TO_HOME
+ * @property {Symbol} RETURN_TO_SECONDARY_HOME
+ * @type {Enum}
+ */
+const NonModalCommand = new Enum("DWELL", "RETURN_TO_HOME", "RETURN_TO_SECONDARY_HOME"); // G4, G28, G30
+
+/**
+ * @property {Symbol} RAPID
+ * @property {Symbol} LINEAR
+ * @property {Symbol} CW_ARC
+ * @property {Symbol} CCW_ARC
+ * @property {Symbol} PROBE_TOWARD_STOP_OR_ERROR
+ * @property {Symbol} PROBE_TOWARD_STOP
+ * @property {Symbol} PROBE_AWAY_STOP_OR_ERROR
+ * @property {Symbol} PROBE_AWAY_STOP
+ * @type {Enum}
+ */
+const MotionMode = new Enum("RAPID", "LINEAR", "CW_ARC", "CCW_ARC", "PROBE_TOWARD_STOP_OR_ERROR", "PROBE_TOWARD_STOP", "PROBE_AWAY_STOP_OR_ERROR", "PROBE_AWAY_STOP"); // G0 G1 G2 G3 G38.2 G38.3 G38.4 G38.5
+
+/**
+ * @property {Symbol} INVERSE_TIME
+ * @property {Symbol} UNITS_PER_MINUTE
+ * @type {Enum}
+ */
+const FeedRateMode = new Enum("INVERSE_TIME", "UNITS_PER_MINUTE"); // G93 G94
+
+/**
+ * @property {Symbol} IMPERIAL
+ * @property {Symbol} METRIC
+ * @type {Enum}
+ */
+const UnitMode = new Enum("IMPERIAL", "METRIC"); // G20 G21
+
+/**
+ * @property {Symbol} ABSOLUTE
+ * @property {Symbol} RELATIVE
+ * @type {Enum}
+ */
+const DistanceMode = new Enum("ABSOLUTE", "RELATIVE"); //G90 G91
+
+/**
+ * @property {Symbol} RELATIVE
+ * @type {Enum}
+ */
+const ArcDistanceMode = new Enum("RELATIVE"); // G91.1
+
+
+const GCodeMap = {
+    G0: MotionMode.RAPID,
+    G1: MotionMode.LINEAR,
+    G2: MotionMode.CW_ARC,
+    G3: MotionMode.CCW_ARC,
+    G4: NonModalCommand.DWELL,
+    G20: UnitMode.IMPERIAL,
+    G21: UnitMode.METRIC,
+    G28: NonModalCommand.RETURN_TO_HOME,
+    G30: NonModalCommand.RETURN_TO_SECONDARY_HOME,
+    "G38.2": NonModalCommand.PROBE_TOWARD_STOP_OR_ERROR,
+    "G38.3": NonModalCommand.PROBE_TOWARD_STOP,
+    "G38.4": NonModalCommand.PROBE_AWAY_STOP_OR_ERROR,
+    "G38.5": NonModalCommand.PROBE_AWAY_STOP,
+    G90: DistanceMode.ABSOLUTE,
+    G91: DistanceMode.RELATIVE,
+    "G91.1": ArcDistanceMode.RELATIVE,
+    G93: FeedRateMode.INVERSE_TIME,
+    G94: FeedRateMode.UNITS_PER_MINUTE
+
+};
+
+class GParser {
+    static parseLines(line) {
+
+    }
+    static interpretLine(line) {
+        line = line.replace(regexClean, "");
+    }
+}
+
+
+/*
+
 UNITS_METRIC = 20;
 UNITS_IMPERIAL = 21;
 
@@ -106,3 +196,5 @@ export default class GCodeConverter {
         return paths;
     }
 }
+
+*/
